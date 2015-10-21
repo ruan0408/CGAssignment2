eb0008f0c083c9076d6912a6865434c710ab5e4b
Gervasio Santos <gervasio.neto94@gmail.com>
2015-10-21 18:41:01 +1100

    Tardis dosen't light up when seen from inside in first person

--- src/fragmentShaderNight.glsl
+++ src/fragmentShaderNight.glsl
@@ -11,6 +11,9 @@ void main() {
     lightDir = vec3(gl_LightSource[1].position-v);
     dist = length(lightDir);
     gl_FragColor = (2.0/(dist*dist))*texture2D(colorMap, gl_TexCoord[0].st);
+    if(dist < 1.0){
+        gl_FragColor = dist*texture2D(colorMap, gl_TexCoord[0].st);
+    }
 
     //This would implement replace mode
     //gl_FragColor = texture2D(colorMap,texCoord);
